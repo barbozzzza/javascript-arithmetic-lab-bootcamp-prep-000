07b3674eb604f17ea6e4238c393bfff95b7f3dc2
jose barboza <jrbarboza08@gmail.com>
2018-06-10 21:07:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 function add(a,b){
   return a+b
 }
+
+function subtract(a,b){
+  return a-b
+}
+
+function multiply(a,b){
+  return a*b
+}
+
+function devide(a,b){
+  return a/b
+}
+
+function inc(n){
+  return n++
+}
+
+function dec(n){
+  return n--
+}
+
+function makeInt(n){
+  
+  return parseInt('n',10)
+}
